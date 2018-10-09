29ec35bcf8c89e86c1211a7e6e6d191ed2079f8c
Mathias Denichi <mathias.denise@ochtec.com>
2018-10-08 21:50:36 -0400

    First pull for data

--- src/components/App/Index.js
+++ src/components/App/Index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import _ from 'lodash'
 
 import './App.css'
 
 class App extends Component {
+  state = {
+    questions: [],
+    score: [],
+  }
+  componentDidMount() {
+    this.fetchQuestions()
+  }
+
+  fetchQuestions = async () => {
+    try {
+      const res = await axios.get('https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean')
+      const { data } = res
+      return this.setState({
+        questions: data.results,
+      })
+    } catch (err) {
+      return err
+    }
+  }
   render() {
+    const { questions } = this.state
+    console.log(questions)
     return (
       <p>App</p>
     )
